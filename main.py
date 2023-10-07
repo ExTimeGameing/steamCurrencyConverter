c2aae2bcee0539829ccc49a4212b93427a88a77d
epbugrov <epbugrov@sberdevices.ru>
2023-10-07 13:19:50 +0300

    Feature/001: Перенос расчетов после ввода Добавление паузы в конце выполнения

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Блок импортов
 from requests_html import HTMLSession
+import os
 
 # Блок объявления переменных
 priceCartRUB = 0.0
@@ -7,20 +8,24 @@ priceCartUSD = 0.0
 UsdRubConvert = 0.0
 textFinder = "RUB"
 
-# Шаг 1 Получаем курс 1 доллар в рублях в стиме
+# Шаг 1 Получаем размер корзины в рублях
+priceCartRUB = float(input("Введите размер вашей корзины в рублях используя символ . для десятых и сотых << "))
+
+# Шаг 2 Получаем курс 1 доллар в рублях в стиме
 session = HTMLSession()
 response = session.get("https://t.me/s/steamrub")
 substringID = response.text.rfind(textFinder)
-currencyStr = response.text[substringID-7:substringID-1]
+currencyStr = response.text[substringID - 7:substringID - 1]
 if currencyStr[0] == " ":
     currencyStr = currencyStr[1:]
 UsdRubConvert = float(currencyStr)
 
-# Шаг 2 Получаем размер корзины в рублях и высчитываем корзину в долларах
-priceCartRUB = float(input("Введите размер вашей корзины в рублях используя символ . для десятых и сотых << "))
+# Шаг 3 Вычисляем корзину в долларах
 priceCartUSD = priceCartRUB / UsdRubConvert
 
-# Шаг 3 Выводим информацию пользователю
+# Шаг 4 Выводим информацию пользователю
 print(f"Текущий курс 1 доллара к рублю >> {UsdRubConvert} RUB/USD\n"
       f"Ваша корзина в рублях >> {priceCartRUB} RUB\n"
       f"Ваша корзина в долларах для Киви >> {priceCartUSD} USD, Округляя до двух знаков  >> {round(priceCartUSD, 2)} $")
+
+os.system("pause")
